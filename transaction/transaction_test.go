980773a6df39aa9010eae7cb0174eb4c82885436
saklani <50768838+saklani@users.noreply.github.com>
2024-06-20 14:16:35 +0530

    tests: more test for transaction test

--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/liteseed/goar/signer"
+	"github.com/liteseed/goar/tag"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,10 +14,23 @@ func TestSign(t *testing.T) {
 	s, err := signer.FromPath("../test/signer.json")
 	assert.NoError(t, err)
 
-	tx := New(data, "", "0", nil)
-	assert.NoError(t, err)
-
 	t.Run("Sign", func(t *testing.T) {
+		tx := New(data, "", "0", nil)
+		assert.NoError(t, err)
+		tx.Owner = s.Owner()
+		tx.LastTx = "lqsw6xgaaunfs8h3d6n54ci1lgm2tmtqvz3wke9v9ygq64q8s68yz2jfq5xy4nec"
+		tx.Reward = "1000"
+
+		err = tx.Sign(s)
+		assert.NoError(t, err)
+
+		err = tx.Verify()
+		assert.NoError(t, err)
+	})
+
+	t.Run("Sign with Tags", func(t *testing.T) {
+		tx := New(data, "", "0", []tag.Tag{{Name: "test", Value: "test"}, {Name: "test", Value: "test"}})
+		assert.NoError(t, err)
 		tx.Owner = s.Owner()
 		tx.LastTx = "lqsw6xgaaunfs8h3d6n54ci1lgm2tmtqvz3wke9v9ygq64q8s68yz2jfq5xy4nec"
 		tx.Reward = "1000"
